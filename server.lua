79129954ab106c492dfa7060be5d02d2e8631f9e
itomwav <79104721+itomwav@users.noreply.github.com>
2022-08-09 01:47:40 +0200

    bugfixes

--- server.lua
+++ server.lua
@@ -6,25 +6,29 @@ table.insert(map,1,{})
 
 while true do
 local id,message,protocol = rednet.receive()
-if protocol == req then
-    x = string.sub(message,0,string.find(message," ")-1)
-    y = string.sub(message,string.find(message," ")+1,string.len(message))
+if protocol == "req" then
+    x = tonumber(string.sub(message,0,string.find(message," ")-1))
+    y = tonumber(string.sub(message,string.find(message," ")+1,string.len(message)))
 
     if y == 0 then
         if map[x-1] ~= nil then
-        print("mining"..x.." "..y)
+        print("mining "..x.." "..y)
+        end
+    elseif math.abs(y) = 1 then --wegen unsauberer Programmierung
+        if map[x] ~= "mining" and map[x] ~= nil then
+        print("mining "..x.." "..y)
         end
     elseif y < 0 then
-        if map[x][y+1] == "mined" && map[x][y] ~= "mining" then
-        print("mining"..x.." "..y)
+        if map[x][y+1] == "mined" and map[x][y] ~= "mining" then
+        print("mining "..x.." "..y)
         end
     elseif y > 0 then
-        if map[x][y-1] == "mined" && map[x][y] ~= "mining" then
-        print("mining"..x.." "..y)
+        if map[x][y-1] == "mined" and map[x][y] ~= "mining" then
+        print("mining "..x.." "..y)
         end
     end
 
-elseif protocol == change then
+elseif protocol == "change" then
     x = string.sub(message,0,string.find(message," ")-1)
     y = string.sub(message,string.find(message," ")+1,string.len(message))
     if y == 0 then 
