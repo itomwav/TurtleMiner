9a005e6ff8fc3368de23c68b1f46fa5f7ffb86e0
itomwav <79104721+itomwav@users.noreply.github.com>
2022-08-09 02:07:16 +0200

    further bugfixes

--- server.lua
+++ server.lua
@@ -14,7 +14,7 @@ if protocol == "req" then
         if map[x-1] ~= nil then
         print("mining "..x.." "..y)
         end
-    elseif math.abs(y) = 1 then --wegen unsauberer Programmierung
+    elseif math.abs(y) == 1 then --wegen unsauberer Programmierung
         if map[x] ~= "mining" and map[x] ~= nil then
         print("mining "..x.." "..y)
         end
@@ -29,12 +29,14 @@ if protocol == "req" then
     end
 
 elseif protocol == "change" then
-    x = string.sub(message,0,string.find(message," ")-1)
-    y = string.sub(message,string.find(message," ")+1,string.len(message))
+    x = tonumber(string.sub(message,0,string.find(message," ")-1))
+    y = tonumber(string.sub(message,string.find(message," ")+1,string.len(message)))
     if y == 0 then 
         table.insert(map,x,{})
+        print("mined "..x.." "..y)
     else
         table.insert(map[x],y,"mined")
+        print("mined "..x.." "..y)
     end
 else
     rednet.send(tonumber(protocol),message,tostring(id))
