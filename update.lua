09eb9220407ef45035ffdbda7874a5d4a7d30643
itomwav <79104721+itomwav@users.noreply.github.com>
2022-08-12 00:31:11 +0200

    fixed device detection

--- update.lua
+++ update.lua
@@ -3,10 +3,12 @@ shell.run("rm", file)
 shell.run("wget", "https://github.com/itomwav/TurtleMiner/raw/main/"..file)
 end
 
-if turtle == nil then --Turtle
-get("miner.lua")
-elseif pocket then --Tablet
-get("tablet.lua")
-else --Computer -> Server
-get("server.lua")
+if turtle == nil then 
+    if pocket then
+        get("tablet.lua") --Tablet
+    else
+        get("server.lua") --Server
+    end
+else 
+    get("miner.lua") --Turtle 
 end
